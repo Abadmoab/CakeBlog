9e7340698a84f85cc504f6ee836e009802719909
Abdullah Abdelqader <abadmoab@gmail.com>
2020-12-02 10:45:48 +0200

    Create post view ctp file

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -8,4 +8,13 @@ class PostsController extends AppController
 	{
 		$this->set('posts', $this->Post->find('all'));
 	}
+
+	public function view($post = null)
+	{
+		if (!$post = $this->Post->findById($post)) {
+			throw new NotFoundException('Invalid post.');
+		}
+
+		$this->set('post', $post);
+	}
 }
